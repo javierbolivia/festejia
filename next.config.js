e168124710a28ed99fe31ed3f0424d1fb6088093
javierbolivia <javierbolivia@users.noreply.github.com>
2026-07-25 03:28:29 -0400

    Fix: agregar trailingSlash para que funcionen las sub-páginas en static export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   output: 'export',
   distDir: 'out',
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
